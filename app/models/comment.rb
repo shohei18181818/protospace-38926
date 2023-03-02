90521dd0f904863a8966ef5ee78c2dd85ad457e7
shohei18181818 <hob2509@yahoo.co.jp>
2023-03-02 20:33:17 +0900

    Revert "initial commit"
    This reverts commit 6b97fc8d85be518f1df7cd0a7a412ee501d8aeb3.

--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :prototype
-  belongs_to :user
-
-  validates :content, presence: true
-  validates :prototype, presence: true
-  validates :user, presence: true
-end
